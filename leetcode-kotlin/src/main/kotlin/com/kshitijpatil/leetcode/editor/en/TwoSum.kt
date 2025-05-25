0a0d0dae019fe2e75ee3ba0d72b57c7d3960060f
Kshitij Patil <kshitijpatil98@gmail.com>
2025-05-26 01:57:17 +0530

    LC Kotlin: Add TwoSum soln
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

---
+++ leetcode-kotlin/src/main/kotlin/com/kshitijpatil/leetcode/editor/en/TwoSum.kt
@@ -0,0 +1,71 @@
+package com.kshitijpatil.leetcode.editor.en;
+
+//Given an array of integers nums and an integer target, return indices of the 
+//two numbers such that they add up to target. 
+//
+// You may assume that each input would have exactly one solution, and you may 
+//not use the same element twice. 
+//
+// You can return the answer in any order. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [3,2,4], target = 6
+//Output: [1,2]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [3,3], target = 6
+//Output: [0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 2 <= nums.length <= 10⁴ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// Only one valid answer exists. 
+// 
+//
+// 
+//Follow-up: Can you come up with an algorithm that is less than 
+//O(n²)
+// time complexity?
+//
+// Related Topics Array Hash Table 👍 61699 👎 2227
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val index = hashMapOf<Int, Int>()
+        for (i in nums.indices) {
+            val extra = target - nums[i]
+            val otherIdx = index[extra]
+            if (otherIdx != null) {
+                return intArrayOf(otherIdx, i)
+            }
+            index[nums[i]] = i
+        }
+        error("Invalid input, no matching pair")
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+fun main() {
+    val solution = Solution()
+}
